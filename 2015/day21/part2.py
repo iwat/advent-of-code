2f30d48d1ad097c77d35313d24cd2ac245a02a56
Chaiwat S <iwat@users.noreply.github.com>
2024-12-31 18:11:29 -0800

    feat(2025day21): done

---
+++ 2015/day21/part2.py
@@ -0,0 +1,68 @@
+from itertools import combinations, product
+
+weapons = {
+    'Dagger': {'cost': 8, 'dmg': 4, 'armor': 0},
+    'Shortsword': {'cost': 10, 'dmg': 5, 'armor': 0},
+    'Warhammer': {'cost': 25, 'dmg': 6, 'armor': 0},
+    'Longsword': {'cost': 40, 'dmg': 7, 'armor': 0},
+    'Greataxe': {'cost': 74, 'dmg': 8, 'armor': 0},
+}
+
+armors = {
+    'Bare': {'cost': 0, 'dmg': 0, 'armor': 0},
+    'Leather': {'cost': 13, 'dmg': 0, 'armor': 1},
+    'Chainmail': {'cost': 31, 'dmg': 0, 'armor': 2},
+    'Splintmail': {'cost': 53, 'dmg': 0, 'armor': 3},
+    'Bandedmail': {'cost': 75, 'dmg': 0, 'armor': 4},
+    'Platemail': {'cost': 102, 'dmg': 0, 'armor': 5},
+}
+
+rings = {
+    'NoRing1': {'cost': 0, 'dmg': 0, 'armor': 0},
+    'NoRing2': {'cost': 0, 'dmg': 0, 'armor': 0},
+    'Damage +1': {'cost': 25, 'dmg': 1, 'armor': 0},
+    'Damage +2': {'cost': 50, 'dmg': 2, 'armor': 0},
+    'Damage +3': {'cost': 100, 'dmg': 3, 'armor': 0},
+    'Defense +1': {'cost': 20, 'dmg': 0, 'armor': 1},
+    'Defense +2': {'cost': 40, 'dmg': 0, 'armor': 2},
+    'Defense +3': {'cost': 80, 'dmg': 0, 'armor': 3},
+}
+
+
+def worst_fight(boss_hp, boss_dmg, boss_armor):
+    worst_lose = None
+    worst_combination = None
+    for comb in product(weapons.items(), armors.items(), rings.items(), rings.items()):
+        if comb[2] == comb[3]:
+            continue
+
+        player_hp = 100
+        player_dmg = sum([props['dmg'] for name, props in comb])
+        player_armor = sum([props['armor'] for name, props in comb])
+        cost = sum([props['cost'] for name, props in comb])
+
+        winner = fight(player_hp, player_dmg, player_armor, boss_hp, boss_dmg, boss_armor)
+
+        if winner == 'Boss':
+            print('Boss WON !!!', cost, *[name for name, _ in comb])
+            if worst_lose is None or cost > worst_lose:
+                worst_lose = cost
+                worst_combination = tuple([name for name, _ in comb])
+
+    return (worst_combination, worst_lose)
+
+def fight(player_hp, player_dmg, player_armor, boss_hp, boss_dmg, boss_armor):
+    turn = 0
+    while player_hp > 0 and boss_hp > 0:
+        if turn == 0:
+            boss_hp -= max(1, player_dmg - boss_armor)
+        else:
+            player_hp -= max(1, boss_dmg - player_armor)
+        turn = (turn + 1) % 2
+    if player_hp > 0:
+        return 'Player'
+    return 'Boss'
+
+
+if __name__ == '__main__':
+    print(worst_fight(boss_hp=109, boss_dmg=8, boss_armor=2))
