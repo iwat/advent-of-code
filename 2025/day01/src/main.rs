8b959233ff7ce6bb3b6d6db089215d4db2b1b95f
Chaiwat S <iwatth@gmail.com>
2025-12-01 18:48:09 -0800

    feat: 2025 day 1 part 2

--- 2025/day01/src/main.rs
+++ 2025/day01/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 
 const MAX_POSITION: i32 = 100;
 
+#[allow(dead_code)]
 fn part1() -> io::Result<()> {
     let contents = fs::read_to_string("input.txt")?;
     let lines = contents.lines();
@@ -33,6 +34,41 @@ fn part1() -> io::Result<()> {
     Ok(())
 }
 
+fn part2() -> io::Result<()> {
+    let contents = fs::read_to_string("input.txt")?;
+    let lines = contents.lines();
+
+    let mut position = 50;
+    let mut clicks = 0;
+    for line in lines {
+        let direction = line.chars().next().unwrap();
+        let distance = line[1..].parse::<i32>().unwrap();
+        let before = position;
+
+        if direction == 'R' {
+            position += distance;
+            clicks += position / MAX_POSITION;
+        } else if direction == 'L' {
+            position -= distance;
+            if before > 0 && position <= 0 {
+                clicks += 1;
+            }
+            clicks += -position / MAX_POSITION;
+            if position < 0 {
+                position += MAX_POSITION * (-position / MAX_POSITION + 1);
+            }
+        }
+        position %= MAX_POSITION;
+
+        println!(
+            "{} {}{} {} {}",
+            before, direction, distance, position, clicks
+        );
+    }
+    println!("Clicks: {}", clicks);
+    Ok(())
+}
+
 fn main() -> io::Result<()> {
-    part1()
+    part2()
 }
