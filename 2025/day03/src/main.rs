7b9a4101603a1245c5b172e853782ad0c692d578
Chaiwat S <iwatth@gmail.com>
2025-12-04 18:48:47 -0800

    feat: 2025 day 3 part 2
    Add part2 function to process input and sum joltage values
    
    Enable backtrace in terminal settings for debugging

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -3,6 +3,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+#[allow(dead_code)]
 fn part1() -> Result<(), std::io::Error> {
     let file = File::open("input.txt")?;
     let file = BufReader::new(file);
@@ -36,6 +37,41 @@ fn part1() -> Result<(), std::io::Error> {
     }
     Ok(())
 }
+
+fn part2() -> Result<(), std::io::Error> {
+    let file = File::open("input.txt")?;
+    let file = BufReader::new(file);
+    let mut sum: u64 = 0;
+    for line in file.lines() {
+        let line = line?;
+        let line = line.trim();
+
+        let vals: Vec<u8> = line
+            .chars()
+            .filter_map(|c| c.to_digit(10).map(|d| d as u8))
+            .collect();
+        println!("line {:?}", vals);
+
+        let mut joltage: u64 = 0;
+        let mut digit: (i32, &u8) = (-1, &0);
+        for exp in (0..=11).rev() {
+            let next_digit = vals[(digit.0 + 1) as usize..vals.len() - exp]
+                .iter()
+                .enumerate()
+                .max_by_key(|(idx, val)| (*val, -(*idx as i32)))
+                .unwrap();
+            digit = (digit.0 + 1 + next_digit.0 as i32, next_digit.1);
+            println!("exp {} digit {:?}", exp, digit);
+            joltage += *digit.1 as u64 * 10u64.pow(exp as u32);
+        }
+
+        let old_sum = sum;
+        sum += joltage;
+        println!("Sum {} + {} = {}", old_sum, joltage, sum);
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error> {
-    part1()
+    part2()
 }
