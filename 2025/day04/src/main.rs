75b4477d34d7d7d96895cca02e829bb19b65d064
Chaiwat S <iwatth@gmail.com>
2025-12-04 20:17:58 -0800

    feat: 2025 day 4 part 2

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -1,12 +1,5 @@
 use std::fs;
 
-#[allow(dead_code)]
-fn print_grid(grid: &Vec<Vec<char>>) {
-    for row in grid {
-        println!("{}", row.iter().collect::<String>());
-    }
-}
-
 fn count_neighbors(x: usize, y: usize, grid: &Vec<Vec<char>>) -> u8 {
     let mut count = 0;
     for dx in -1..=1 {
@@ -26,6 +19,7 @@ fn count_neighbors(x: usize, y: usize, grid: &Vec<Vec<char>>) -> u8 {
     count
 }
 
+#[allow(dead_code)]
 fn part1() -> Result<(), std::io::Error> {
     let file = fs::read_to_string("input.txt")?;
     let file = file.trim();
@@ -37,7 +31,6 @@ fn part1() -> Result<(), std::io::Error> {
     let height = grid.len();
     let mut accessibles = 0;
     println!("WxH {}x{}", width, height);
-    //print_grid(&grid);
     for y in 0..height {
         for x in 0..width {
             if grid[y][x] == '@' {
@@ -54,6 +47,48 @@ fn part1() -> Result<(), std::io::Error> {
     Ok(())
 }
 
+fn part2() -> Result<(), std::io::Error> {
+    let file = fs::read_to_string("input.txt")?;
+    let file = file.trim();
+    let mut grid = file
+        .split("\n")
+        .map(|line| line.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    let width = grid[0].len();
+    let height = grid.len();
+    let mut removed = 0;
+    println!("WxH {}x{}", width, height);
+    loop {
+        println!("---");
+        let mut removed_in_loop = 0;
+        for y in 0..height {
+            for x in 0..width {
+                if grid[y][x] == '@' {
+                    if count_neighbors(x, y, &grid) <= 3 {
+                        grid[y][x] = 'x';
+                        removed_in_loop += 1;
+                    }
+                }
+                print!("{}", grid[y][x]);
+            }
+            println!();
+        }
+        for y in 0..height {
+            for x in 0..width {
+                if grid[y][x] == 'x' {
+                    grid[y][x] = '.';
+                }
+            }
+        }
+        removed += removed_in_loop;
+        if removed_in_loop == 0 {
+            break;
+        }
+    }
+    println!("Removed cells: {}", removed);
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error> {
-    part1()
+    part2()
 }
