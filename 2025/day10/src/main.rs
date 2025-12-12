e87291fea695e774b4482a7631d00ee6a6bae07f
Chaiwat S <iwatth@gmail.com>
2025-12-11 19:24:54 -0800

    feat(wip): 2025 day 10 part 1

---
+++ 2025/day10/src/main.rs
@@ -0,0 +1,117 @@
+use core::fmt;
+use std::{error::Error, fs};
+
+struct BinaryVec<'a> {
+    data: &'a Vec<u32>,
+    bits: usize,
+}
+
+impl<'a> fmt::Debug for BinaryVec<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("[")?;
+
+        for (i, item) in self.data.iter().enumerate() {
+            write!(f, "{:0w$b}", item, w = self.bits)?;
+            if i < self.data.len() - 1 {
+                f.write_str(", ")?;
+            }
+        }
+        f.write_str("]")
+    }
+}
+
+struct Machine {
+    bits: u8,
+    goal: u32,
+    buttons: Vec<u32>,
+    joltages: Vec<u32>,
+}
+
+impl fmt::Debug for Machine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Machine")
+            .field(
+                "goal",
+                &format_args!("{:0w$b}", self.goal, w = self.bits as usize),
+            )
+            .field(
+                "buttons",
+                &BinaryVec {
+                    data: &self.buttons,
+                    bits: self.bits as usize,
+                },
+            )
+            .field("joltages", &self.joltages)
+            .finish()
+    }
+}
+
+impl Machine {
+    fn new(goal_str: &str) -> Self {
+        let mut goal = 0u32;
+        for c in goal_str[1..goal_str.len() - 1].chars() {
+            if c == '#' {
+                goal = goal << 1;
+                goal |= 1;
+            } else {
+                goal = goal << 1;
+            }
+        }
+
+        Machine {
+            bits: goal_str.len() as u8 - 2,
+            goal: goal,
+            buttons: Vec::new(),
+            joltages: Vec::new(),
+        }
+    }
+
+    fn add_button(&mut self, b: &str) {
+        let mut button = 0u32;
+        for c in b[1..b.len() - 1].split(',') {
+            let c8 = c.parse::<u8>().unwrap();
+            button |= 1 << (self.bits - c8 - 1);
+        }
+        self.buttons.push(button);
+    }
+
+    fn set_joltages(&mut self, v: &str) {
+        self.joltages = v[1..v.len() - 1]
+            .split(',')
+            .map(|vv| vv.parse().unwrap())
+            .collect();
+    }
+}
+
+fn read_input() -> Result<Vec<Machine>, Box<dyn Error>> {
+    let file = fs::read_to_string("simple-input.txt")?;
+    let mut machines = Vec::<Machine>::new();
+
+    for line in file.lines() {
+        let line = line.trim();
+        if line == "" {
+            continue;
+        }
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        let mut m = Machine::new(parts[0]);
+        for i in 1..parts.len() - 1 {
+            m.add_button(parts[i]);
+        }
+        m.set_joltages(parts[parts.len() - 1]);
+        machines.push(m);
+    }
+
+    Ok(machines)
+}
+
+fn part1() -> Result<(), Box<dyn Error>> {
+    let machines = read_input()?;
+    for m in machines {
+        println!("{:?}", m);
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    part1()
+}
