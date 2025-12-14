293f53cf44539c404f69b941bb5b032291adb4de
Chaiwat S <iwatth@gmail.com>
2025-12-13 23:10:37 -0800

    feat: 2025 day 11 part 2 (iterative-memoization)

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -2,8 +2,10 @@ use std::{
     collections::{HashMap, VecDeque},
     fs::File,
     io::{BufRead, BufReader},
+    ops::AddAssign,
 };
 
+#[allow(dead_code)]
 fn read_input() -> Result<HashMap<String, Vec<String>>, std::io::Error> {
     let file = File::open("input.txt")?;
     let buf = BufReader::new(file);
@@ -33,6 +35,7 @@ fn read_input() -> Result<HashMap<String, Vec<String>>, std::io::Error> {
     Ok(map)
 }
 
+#[allow(dead_code)]
 fn solve1(map: &HashMap<String, Vec<String>>) {
     let mut queue = VecDeque::<&str>::new();
     queue.push_front("you");
@@ -52,12 +55,72 @@ fn solve1(map: &HashMap<String, Vec<String>>) {
     println!("ways: {}", ways);
 }
 
+#[allow(dead_code)]
 fn part1() -> Result<(), std::io::Error> {
     let map = read_input()?;
     solve1(&map);
     Ok(())
 }
 
+fn solve2(map: &HashMap<String, Vec<String>>) {
+    let mut counter = HashMap::<(&str, bool, bool), usize>::new();
+    let mut queue = VecDeque::<(&str, bool, bool)>::new();
+    queue.push_front(("svr", false, false));
+
+    while let Some((next, seen_fft, seen_dac)) = queue.pop_back() {
+        // println!("Processing {} {} {}", next, seen_fft, seen_dac);
+        if next == "out" {
+            let c = counter.entry((next, seen_fft, seen_dac)).or_insert(0);
+            if seen_fft && seen_dac {
+                c.add_assign(1);
+            }
+            // println!("set counter: {:?} = {}", (next, seen_fft, seen_dac), c);
+            continue;
+        }
+        if let Some(_c) = counter.get(&(next, seen_fft, seen_dac)) {
+            // println!("mem counter: {:?} = {}", (next, seen_fft, seen_dac), c);
+            continue;
+        }
+        let children = map.get(next).unwrap();
+
+        let mut res = 0;
+        let mut all_children = true;
+        for child in children {
+            match counter.get(&(
+                child,
+                seen_fft || child == "fft",
+                seen_dac || child == "dac",
+            )) {
+                Some(child_res) => res += child_res,
+                None => all_children = false,
+            }
+        }
+        if all_children {
+            let c = counter.entry((next, seen_fft, seen_dac)).or_insert(0);
+            c.add_assign(res);
+            // println!("set counter: {:?} = {}", (next, seen_fft, seen_dac), c);
+            continue;
+        } else {
+            queue.push_back((next, seen_fft, seen_dac));
+        }
+        for child in children {
+            queue.push_back((
+                child,
+                seen_fft || child == "fft",
+                seen_dac || child == "dac",
+            ));
+        }
+    }
+
+    println!("counter: {:?}", counter[&("svr", false, false)]);
+}
+
+fn part2() -> Result<(), std::io::Error> {
+    let map = read_input()?;
+    solve2(&map);
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error> {
-    part1()
+    part2()
 }
